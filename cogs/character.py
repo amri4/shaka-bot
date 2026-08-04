062bb198c87a357a710ac631bf4c2817f9fa1481
amri4 <amri.supreme123@gmail.com>
2026-08-04 19:55:29 +0300

    Add welcome message for new members
    Send a welcome embed message when a new member joins.

--- cogs/character.py
+++ cogs/character.py
@@ -10,3 +10,8 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        channel = discord.utils.get(member.guild.channels, name="🏯-〘𝗘𝗡𝗧𝗥𝗔𝗡𝗖𝗘-𝗚𝗔𝗧𝗘")
+        if channel is None:
+            return
+
+        embed = discord.Embed(title="🌊 The Waves Carry a New Arrival", description=f"""**Welcome to [guild.name]**\n\n 

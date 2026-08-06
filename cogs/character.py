60add03e793b292c2842a3a141a5c8970dc5430b
amri4 <amri.supreme123@gmail.com>
2026-08-06 12:33:53 +0300

    Refactor claims table creation into Claim class

--- cogs/character.py
+++ cogs/character.py
@@ -1,13 +1,23 @@
+from discord.ext import commands
 import mycord
 
 db = mycord.DB()
 
-db.create_table(
-    "claims",
-    """
-    user_id INTEGER PRIMARY KEY,
-    character TEXT UNIQUE
-    """
-)
 
-print("Claims table ready!")
+class Claim(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        db.create_table(
+            "claims",
+            """
+            user_id INTEGER PRIMARY KEY,
+            character TEXT UNIQUE
+            """
+        )
+
+        print("Claims table ready!")
+
+
+async def setup(bot):
+    await bot.add_cog(Claim(bot))

d664f89dafcf4616accff08665f6daea54fd3d7d
amri4 <amri.supreme123@gmail.com>
2026-08-11 00:55:06 +0300

    Fix claim message formatting in character.py

--- cogs/character.py
+++ cogs/character.py
@@ -252,12 +252,16 @@ class Claim(commands.Cog):
             message = await channel.fetch_message(data[2])
 
             claims = db.fetchall("claims")
+            claims = [
+                claim for claim in claims
+                if claim[0] == ctx.guild.id
+            ]
 
             if claims:
                 description = ""
 
                 for claim in claims:
-                    description += f"• <@{claim[0]}> — **{claim[1]}**\n"
+                    description += f"• <@{claim[1]}> — **{claim[2]}**\n"
             else:
                 description = "No characters have been claimed yet."
 

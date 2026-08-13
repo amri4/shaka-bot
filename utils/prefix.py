14639d89dfae9ecf0ddfc1f536cce8fe5aa80363
amri4 <amri.supreme123@gmail.com>
2026-08-13 13:50:51 +0300

    Delete utils directory

--- utils/prefix.py
+++
@@ -1,7 +0,0 @@
-from config import BOT_PREFIX
-
-def get_prefix(bot, message):
-    if message.content.lower().startswith(BOT_PREFIX.lower()):
-        return message.content[:len(BOT_PREFIX)]
-
-    return []

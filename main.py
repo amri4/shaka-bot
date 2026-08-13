ccf84a95782efbc6252ae16356d846c580164dc5
amri4 <amri.supreme123@gmail.com>
2026-08-13 13:36:28 +0300

    Change bot command prefix to use dynamic function

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import mycord
+from utils.prefix import get_prefix
 
 intents = discord.Intents.default()
 intents.message_content = True  
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, intents=intents)
 
 async def load_extensions():
     print("📂 Scanning for cogs...")

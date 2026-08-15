ef4587dc7874e27680947c991a5b0da0e4a7fb53
amri4 <amri.supreme123@gmail.com>
2026-08-15 23:05:37 +0300

    Enhance command decorator with usage parameter
    Added optional usage parameter to command decorator.

--- utils/command.py
+++ utils/command.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 
-def command(category, description=None, **kwargs):
+def command(category, description=None, usage=None, **kwargs):
     def decorator(func):
 
         cmd = commands.command(
@@ -9,7 +9,12 @@ def command(category, description=None, **kwargs):
             **kwargs
         )(func)
 
+        # Help menu category
         cmd.extras["help_category"] = category
+
+        # Optional custom usage
+        if usage is not None:
+            cmd.extras["help_usage"] = usage
 
         return cmd
 

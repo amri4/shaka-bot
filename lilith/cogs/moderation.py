d0f9f8465c678e7edec0683c875dd4526b17582f
amri4 <amri.supreme123@gmail.com>
2026-08-18 19:02:14 +0300

    Update moderation.py

--- lilith/cogs/moderation.py
+++ lilith/cogs/moderation.py
@@ -4,3 +4,21 @@ import mycord
 from utils.command import command
 
 db = mycord.PunksDB()
+
+db.create_table(
+    "cases",
+    """
+    case_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    guild_id INTEGER NOT NULL,
+    reporter_id INTEGER NOT NULL,
+    suspect_id INTEGER NOT NULL,
+    status TEXT NOT NULL,
+    created_at TEXT NOT NULL
+    """
+)
+CASE_STATUSES = {
+    "OPEN",
+    "REVIEWING",
+    "RESOLVED",
+    "DISMISSED"
+}

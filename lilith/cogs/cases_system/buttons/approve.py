90c219614b99153d68fc2b1edb548a2cd021c9c2
amri4 <amri.supreme123@gmail.com>
2026-08-22 01:11:06 +0300

    Add approve button with response message

---
+++ lilith/cogs/cases_system/buttons/approve.py
@@ -0,0 +1,5 @@
+from utils.discord_setup import *
+
+@button(label="approve", color="green")
+async def approve(interaction, button):
+  await interaction.response.send_message("✅️ Approved", ephermal=True)

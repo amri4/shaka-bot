20c04021104c19954f3ce16daecccb446783976f
amri4 <amri.supreme123@gmail.com>
2026-08-22 18:08:57 +0300

    Add test reaction listener for raw reaction events

---
+++ lilith/cogs/cases_system/reactions/test.py
@@ -0,0 +1,12 @@
+from utils.discord_setup import *
+
+
+@listener("on_raw_reaction_add")
+async def test_reaction(payload):
+
+    print(
+        "🔥🔥 REACTION EVENT FIRED",
+        payload.emoji,
+        payload.user_id,
+        payload.message_id
+    )

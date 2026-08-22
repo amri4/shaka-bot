be12fdaa4b4187c7d68967c94a07b399982aa3eb
amri4 <amri.supreme123@gmail.com>
2026-08-22 18:03:00 +0300

    Add print statements for debugging in ReactionTest

--- lilith/cogs/reaction_test/commands/reaction_test.py
+++ lilith/cogs/reaction_test/commands/reaction_test.py
@@ -6,15 +6,15 @@ class ReactionTest(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        print("🔥 REACTION TEST COG LOADED")
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(
         self,
         payload
     ):
-
         print(
-            "🔥 REACTION DETECTED:",
+            "🔥🔥 REACTION EVENT FIRED",
             payload.emoji,
             payload.user_id,
             payload.message_id

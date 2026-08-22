9a7b2748ba93f1ee3c67855e0ead0ef807c65a29
amri4 <amri.supreme123@gmail.com>
2026-08-23 00:06:32 +0300

    Update test command to use embed and button

--- lilith/cogs/moderation/commands/test.py
+++ lilith/cogs/moderation/commands/test.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
+from ..buttons.test import test_button
 
 
 @commands.command()
 async def test(ctx):
-    await ctx.send("Loader works!")
+    embed = discord.Embed(title="embed works", description="embed, command and loader works", color=discord.Color.green())
+    await ctx.send(embed=embed, view=ui(test_button))

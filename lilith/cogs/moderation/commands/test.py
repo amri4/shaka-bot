9d936dcc7412896b64b9a4aa38101e78f096ba0f
amri4 <amri.supreme123@gmail.com>
2026-08-25 04:17:27 +0300

    Implement number guessing game in test.py

--- lilith/cogs/moderation/commands/test.py
+++ lilith/cogs/moderation/commands/test.py
@@ -1,22 +1,50 @@
 import discord
 from discord.ext import commands
-from ..punishments import *
-punishments = {"👢": kick, "🔇": timeout}
+import random
+import asyncio
 
-def get_punishment(emoji):
-    if emoji in punishments:
-        return punishments[emoji]
-    else:
-        return None
+class NumberGuess(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.active_games = set()
 
-selected = set()
+    def can_start_game(self):
+        people = len(self.active_games)
+        if people >= 5:
+            return False
+        else:
+            return True
 
-@commands.Cog.listener()
-async def on_reaction_add(reaction, user):
-    punishment = get_punishment(reaction.emoji)
-    if punishment is None:
-        return
-    if punishment in selected:
-        selected.remove(punishment)
-    else:
-        selected.add(punishment)
+    @commands.command()
+    async def numberguess(self, ctx):
+        if self.can_start_game():
+            self.active_games.add(ctx.author.id)
+        else:
+            await ctx.send("OH WAIT I'm feeling dizzy right now many people are playing, try again later")
+            return
+        await ctx.send("Guess a number from 1 to 100, i will tell u if your number is HIGHERR or LOWERR!")
+        secret = random.randint(1, 100)
+        while True:
+            try:
+                message = await self.bot.wait_for("message", timeout=20)
+                if message.author.id != ctx.author.id:
+                    continue
+                if not message.content.isdigit():
+                    await ctx.send("say it in numbers not words")
+                    continue
+                guess = int(message.content)
+                if guess > secret:
+                    await ctx.send("A bit lower")
+                elif guess < secret:
+                    await ctx.send("A bit higher")
+                else:
+                    await ctx.send("THAT'S THE NUMBER, YOU WONNN")
+                    self.active_games.remove(ctx.author.id)
+                    break
+            except asyncio.TimeoutError:
+                await ctx.send("time ended")
+                self.active_games.remove(ctx.author.id)
+                break
+
+async def setup(bot):
+    await bot.add_cog(NumberGuess(bot))
